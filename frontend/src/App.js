2b52b60746ca783898b2c1b2a25ca66b179d40fc
simeo russo <simeo94@hotmail.com>
2020-09-18 14:55:35 +0100

    added template into react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,23 +3,114 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const openMenu = () =>{
+    document.querySelector(".sidebar").classList.add('open');
+  }
+
+  const closeMenu = () =>{
+   document.querySelector(".sidebar").classList.remove('open');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div classNameName="App">
+
+  <div className="grid-container">
+    <header className="header">
+      <div className="brand">
+        <button onClick={openMenu} >&#9776;</button>
+        <a href="">Amazona</a>
+      </div>
+      <div className="header-links">
+        <a href="">Cart</a>
+        <a href="">Signin</a>
+      </div>
+    </header>
+
+    <aside className='sidebar'>
+      <h3>Shopping Categories</h3>
+      <button className="sidebar-close-button" onClick={closeMenu}>X</button>
+      <ul>
+        <li>Retro</li>
+        <li>90s</li>
+        <li>Icons</li>
+        <li>Cult Movie</li>
+      </ul>
+    </aside>
+
+
+    <main className="main">
+      <div className="content">
+        <ul className="products">
+          <li>
+            <div className="product">
+              <img src="/images/t-shirt1.jpg" alt="product"/>
+              <div className="product-name"><a href="">Retro shirt</a></div>
+              <div className="product-brand">Nike</div>
+              <div className="product-price">£30</div>
+              <div className="product-rating">4.5 Stars (10 reviews)</div>
+            </div>
+          </li>
+
+          <li>
+            <div className="product">
+              <img src="/images/t-shirt1.jpg" alt="product"/>
+              <div className="product-name"><a href="">Retro shirt</a></div>
+              <div className="product-brand">Nike</div>
+              <div className="product-price">£30</div>
+              <div className="product-rating">4.5 Stars (10 reviews)</div>
+            </div>
+          </li>
+
+          <li>
+            <div className="product">
+              <img src="/images/t-shirt1.jpg" alt="product"/>
+              <div className="product-name"><a href="">Retro shirt</a></div>
+              <div className="product-brand">Nike</div>
+              <div className="product-price">£30</div>
+              <div className="product-rating">4.5 Stars (10 reviews)</div>
+            </div>
+          </li>
+
+          <li>
+            <div className="product">
+              <img src="/images/t-shirt1.jpg" alt="product"/>
+              <div className="product-name"><a href="">Retro shirt</a></div>
+              <div className="product-brand">Nike</div>
+              <div className="product-price">£30</div>
+              <div className="product-rating">4.5 Stars (10 reviews)</div>
+            </div>
+          </li>
+
+          <li>
+            <div className="product">
+              <img src="/images/t-shirt1.jpg" alt="product"/>
+              <div className="product-name"><a href="">Retro shirt</a></div>
+              <div className="product-brand">Nike</div>
+              <div className="product-price">£30</div>
+              <div className="product-rating">4.5 Stars (10 reviews)</div>
+            </div>
+          </li>
+
+          <li>
+            <div className="product">
+              <img src="/images/t-shirt1.jpg" alt="product"/>
+              <div className="product-name"><a href="">Retro shirt</a></div>
+              <div className="product-brand">Nike</div>
+              <div className="product-price">£30</div>
+              <div className="product-rating">4.5 Stars (10 reviews)</div>
+            </div>
+          </li>
+      </ul>
+      </div>
+    </main>
+
+
+    <footer className="footer">
+      All rights reserved.
+    </footer>
+  </div>
+  </div>
   );
 }
 
