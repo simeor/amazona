316209d2b481dfa069dc3292b24e3ddb2e371b2c
simeo russo <simeo94@hotmail.com>
2020-09-22 17:52:13 +0100

    data in express server

---
+++ backend/data.js
@@ -0,0 +1,70 @@
+export default {
+  products: [
+    {
+      _id: 1,
+      name: 'T-shirt',
+      category:'Retro',
+      image: '/images/t-shirt1.jpg',
+      price: 60,
+      brand: 'Geek Online',
+      rating: 4.5,
+      numReviews: 10
+    },
+
+    {
+      _id: 2,
+      name: 'T-shirt',
+      category:'Retro',
+      image: '/images/t-shirt2.jpg',
+      price: 35,
+      brand: 'Hand Prints',
+      rating: 3.5,
+      numReviews: 3
+    },
+
+    {
+      _id: 3,
+      name: 'T-shirt',
+      category:'Retro',
+      image: '/images/t-shirt1.jpg',
+      price: 20,
+      brand: 'Seller 100',
+      rating: 4.1,
+      numReviews: 8
+    },
+
+    {
+      _id: 4,
+      name: 'T-shirt',
+      category:'Retro',
+      image: '/images/t-shirt2.jpg',
+      price: 60,
+      brand: 'Geek Online',
+      rating: 4.5,
+      numReviews: 10
+    },
+
+    {
+      _id: 5,
+      name: 'T-shirt',
+      category:'Retro',
+      image: '/images/t-shirt1.jpg',
+      price: 60,
+      brand: 'Geek Online',
+      rating: 4.5,
+      numReviews: 10
+    },
+
+    {
+      _id: 6,
+      name: 'T-shirt',
+      category:'Retro',
+      image: '/images/t-shirt2.jpg',
+      price: 35,
+      brand: 'Hand Prints',
+      rating: 3.5,
+      numReviews: 3
+    }
+
+  ]
+}
