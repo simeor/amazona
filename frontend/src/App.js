844fd2d59db48e7335791f38694f26b6727c0c36
simeo russo <simeo94@hotmail.com>
2020-09-23 15:26:18 +0100

    tidy up in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,47 +17,47 @@ function App() {
 
 
   return (
-    <BrowserRouter>
+  <BrowserRouter>
+  <div classNameName="App">
 
-    <div classNameName="App">
+    <div className="grid-container">
+      <header className="header">
+        <div className="brand">
+          <button onClick={openMenu} >&#9776;</button>
+          <Link to="/">Amazona</Link>
+        </div>
+        <div className="header-links">
+          <a href="">Cart</a>
+          <a href="">Signin</a>
+        </div>
+      </header>
 
-  <div className="grid-container">
-    <header className="header">
-      <div className="brand">
-        <button onClick={openMenu} >&#9776;</button>
-        <Link to="/">Amazona</Link>
-      </div>
-      <div className="header-links">
-        <a href="">Cart</a>
-        <a href="">Signin</a>
-      </div>
-    </header>
-
-    <aside className='sidebar'>
-      <h3>Shopping Categories</h3>
-      <button className="sidebar-close-button" onClick={closeMenu}>X</button>
-      <ul>
-        <li>Retro</li>
-        <li>90s</li>
-        <li>Icons</li>
-        <li>Cult Movie</li>
-      </ul>
-    </aside>
-
-
-    <main className="main">
-      <div className="content">
-        <Route exact path="/product/:id" component={ProductScreen} />
-        <Route exact path="/" component={HomeScreen} />
-        <Route exact path="/cart/:id?" component={CartScreen} />
-      </div>
-    </main>
+      <aside className='sidebar'>
+        <h3>Shopping Categories</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>X</button>
+        <ul>
+          <li>Retro</li>
+          <li>90s</li>
+          <li>Icons</li>
+          <li>Cult Movie</li>
+        </ul>
+      </aside>
 
 
-    <footer className="footer">
-      All rights reserved.
-    </footer>
-  </div>
+      <main className="main">
+        <div className="content">
+          <Route exact path="/product/:id" component={ProductScreen} />
+          <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/cart/:id?" component={CartScreen} />
+        </div>
+      </main>
+
+
+      <footer className="footer">
+        All rights reserved.
+      </footer>
+    </div>
+
   </div>
   </BrowserRouter>
 
